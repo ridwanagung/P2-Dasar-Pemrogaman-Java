321837429d7fddd27c61c94f549644b87623ae7f
ridwanagung <31800871+ridwanagung@users.noreply.github.com>
2017-09-28 07:04:55 -0700

    Add files via upload

---
+++ Assign.java
@@ -0,0 +1,27 @@
+public class Assign {
+	public static void main (String args[]) {
+		boolean b=true;
+		System.out.println("value b = " + b);
+		
+		char c='C'; //hanya menyediakan satu karakter dan untuk karakter char hannya diberikan petik 1 '...'
+		System.out.println("value c = " + c);
+		
+		byte bt=10;
+		System.out.println("value bt = " + bt);
+		
+		short s=20;
+		System.out.println("value s = " + s);
+		
+		int i=30;
+		System.out.println("value i = " + i);
+		
+		long l=40L;
+		System.out.println("value l = " + l);
+		
+		float f=3.14F; //kalau float ad kom adi tambahi dengan huruf "F"; 
+		System.out.println("value f = " + f);
+		
+		double d=3.14;
+		System.out.println("value d = " + d);
+	}
+}
